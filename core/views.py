0ff96fe2be6bac7f934a995ac4c632822bb6d38d
Ксения Воронина <ksenia.voronina04@yandex.ru>
2026-04-14 21:01:33 +0300

    Реализована главная страница

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-
 def home_view(request):
-    """Отображает главную страницу сайта"""
-    return render(request, 'core/base.html')
+    """Отображает главную страницу"""
+    context = {
+        'active': 'home',  # для подсветки активного пункта меню
+    }
+    return render(request, 'core/home.html', context)
